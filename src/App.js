358119b5a5558b02f6c8212a5781a4d55b9ca08e
Hunter Marion <hunter.orion.marion@outlook.com>
2023-02-18 00:35:18 -0600

    Attempt to align pagedev with Michelle's demos.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Navigation from '../src/components/navigation';
+import Home from '../src/pages/Home';
+import Checkout from '../src/pages/Checkout';
+import Catering from '../src/pages/Catering';
+import Header from '../src/components/header';
+import Footer from '../src/components/footer';
+import About from '../src/pages/About';
+import {
+     BrowserRouter as Router,
+     Routes,
+     Route   } from "react-router-dom";
+import Reservations from './pages/Reservations';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
-  );
+     <div>
+          <Header/>
+          <Router>
+               <Navigation/>
+               <Routes>
+                    <Route exact path="/"  element={<Home />}/>
+                    <Route exact path="/reservations"  element={<Reservations />}/>
+                    <Route exact path="/catering" element={<Catering />} />
+                    <Route exact path="/about" element={<About />}/>
+                    <Route exact path="/checkout" element={<Checkout />} />
+               </Routes>
+          </Router>
+          <Footer/>
+     </div>
+         
+     );
 }
 
 export default App;
